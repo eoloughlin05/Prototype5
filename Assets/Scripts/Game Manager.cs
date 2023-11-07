6e91623a9d8b41185ce49acfef9b58d69cb85b35
eoloughlin05 <eoloughlin05@qub.ac.uk>
2023-11-07 01:46:10 +0000

    5.2 - Keeping Score WIP

--- Assets/Scripts/Game Manager.cs
+++ Assets/Scripts/Game Manager.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     private float spawnRate = 1.0f;
+    private int score;
+    public TextMeshProUGUI scoreText;
 
     void Start()
     {
-        StartCoroutine(SpawnTarget()); 
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
     }
 
     void Update()
@@ -17,12 +22,20 @@ public class GameManager : MonoBehaviour
         
     }
 
+    void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
     IEnumerator SpawnTarget()
     {
         while (true) { 
-        yield return new WaitForSeconds(spawnRate);
-        int index = Random.Range(0, targets.Count);
-        Instantiate(targets[index]);
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+
+            UpdateScore(5);
         }
     }
 }
